f754bba0bbe73aaad60efcc54e5ffb3332528682
Moisés Wendt <moiseswendt@gmail.com>
2021-01-15 17:00:50 -0300

    Add files via upload

---
+++ database/migrations/2021_01_14_133156_create_numbers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNumbersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('numbers', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->foreign('customer_id')->references('id')->on('customers');
+
+            $table->string('number', 14)->nullable();
+
+            $table->enum('status', ['active', 'inactive', 'cancelled'])->default('active')->nullable(false)->comment = "Possiveis status de numeros";
+
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('numbers');
+    }
+}
